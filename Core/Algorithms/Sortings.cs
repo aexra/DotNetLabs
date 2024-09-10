c77a57b176cbffe880dde669cf0546815655a545
Никита Фомин <defdXs@yandex.ru>
2024-09-10 21:39:48 +0300

    Добавьте файлы проекта.

---
+++ Core/Algorithms/Sortings.cs
@@ -0,0 +1,49 @@
+﻿namespace Core.Algorithms;
+
+public class Sortings
+{
+    public static IList<T> BubbleSort<T>(IList<T> items) where T : IComparable<T>
+    {
+        bool sorted;
+
+        do
+        {
+            sorted = true;
+            for (var i = 0; i < items.Count() - 1; i++)
+            {
+                var compare = items[i].CompareTo(items[i + 1]);
+                if (compare > 0)
+                {
+                    var tmp = items[i];
+                    items[i] = items[i + 1];
+                    items[i + 1] = tmp;
+                    sorted = false;
+                }
+            }
+        } while (!sorted);
+
+        return items;
+    }
+    public static IList<T> BubbleSort<T>(IList<T> items, Comparer<T> comparer)
+    {
+        bool sorted;
+
+        do
+        {
+            sorted = true;
+            for (var i = 0; i < items.Count() - 1; i++)
+            {
+                var compare = comparer.Compare(items[i], items[i + 1]);
+                if (compare > 0)
+                {
+                    var tmp = items[i];
+                    items[i] = items[i + 1];
+                    items[i + 1] = tmp;
+                    sorted = false;
+                }
+            }
+        } while (!sorted);
+
+        return items;
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,7 @@
+﻿///
+/// .NET Lab #1
+/// Ver. 3 (19)
+///
+
+Console.WriteLine("Enter int array separated with spaces...");
+Console.WriteLine(string.Join(' ', Core.Algorithms.Sortings.BubbleSort(Console.ReadLine()?.Split(' ').Select(int.Parse).ToList())));
